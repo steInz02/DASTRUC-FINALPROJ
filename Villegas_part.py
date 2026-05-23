db68eab2d6be06e2a05c61650f7b3705811a020d
Nasho <earhlnash02@gmail.com>
2026-05-23 10:37:29 +0800

    Update Villegas_part.py

--- Villegas_part.py
+++ Villegas_part.py
@@ -21,7 +21,7 @@ def delete_movie(self):
         if temp.title.lower() == title.lower():
 
             # CONFIRMATION
-            confirm = input("Delete this movie? (yes/no): ").lower()
+            confirm = input(f"Do you really want to delete '{temp.title}'? (yes/no): ").lower()
 
             if confirm != "yes":
                 print("Delete cancelled.")
